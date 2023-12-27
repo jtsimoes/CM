51e4f65bee733b26ea1fe1d4e413453fc995b20e
João Tomás <jtsimoes@ua.pt>
2023-12-27 02:11:35 +0000

    Fixed minor bug on seed color of app theme

--- Flutter/whatz_up/lib/utils/themes.dart
+++ Flutter/whatz_up/lib/utils/themes.dart
@@ -8,7 +8,7 @@ ThemeData theme(Brightness brightness) {
   return ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0x00128C7E),
+      seedColor: const Color(0xFF128C7E),
       background: brightness == Brightness.light
           ? Colors.white
           : const Color(0xFF051B23),
