06408e65e846235cafef7ddaeedc0c1b4e7ae469
João Tomás <jtsimoes@ua.pt>
2023-12-27 01:26:11 +0000

    Added dynamic extended floating action button to chats page

--- Flutter/whatz_up/lib/pages/chats.dart
+++ Flutter/whatz_up/lib/pages/chats.dart
@@ -1,12 +1,45 @@
 import 'package:whatz_up/utils/globals.dart';
 
-class ChatsPage extends StatelessWidget {
+class ChatsPage extends StatefulWidget {
   const ChatsPage({Key? key}) : super(key: key);
+
+  @override
+  ChatsPageState createState() => ChatsPageState();
+}
+
+class ChatsPageState extends State<ChatsPage> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isExtended = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  _scrollListener() {
+    if (_scrollController.offset >= 50) {
+      setState(() {
+        _isExtended = false;
+      });
+    } else {
+      setState(() {
+        _isExtended = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
+        controller: _scrollController,
         itemCount: 21, // Replace with actual number of chats
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
@@ -23,10 +56,28 @@ class ChatsPage extends StatelessWidget {
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.message),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Padding(
+          padding: _isExtended
+              ? const EdgeInsets.all(0)
+              : const EdgeInsets.only(right: 9.0),
+          child: const Icon(Icons.message),
+        ),
+        label: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: AnimatedSize(
+              duration: const Duration(milliseconds: 200),
+              child: _isExtended
+                  ? const Padding(
+                      padding: EdgeInsets.only(right: 16),
+                      child: Text('Chat nearby friends'),
+                    )
+                  : const Padding(padding: EdgeInsets.all(0))),
+        ),
+        extendedIconLabelSpacing: 0,
+        extendedPadding: const EdgeInsets.only(left: 17.0),
         onPressed: () {
-          // TODO: Navigate to select contact screen
+          print('Find nearby friends screen');
         },
       ),
     );
