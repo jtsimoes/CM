8288809b93714fff47d782d7fbaaa7622d675028
João Tomás <jtsimoes@ua.pt>
2023-12-28 00:57:49 +0000

    Improved calls page with dynamic listing

--- Flutter/whatz_up/lib/pages/calls.dart
+++ Flutter/whatz_up/lib/pages/calls.dart
@@ -1,4 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:whatz_up/utils/globals.dart';
+
+List<List<String>> callHistory = [
+  // Avatar | Phone number | Timestamp | Incoming/outgoing call | Voice/video call]
+  ['A', '(+351) 999 888 777', 'January 5, 13:53', 'in', 'voice'],
+  ['B', '(+351) 666 555 444', 'January 2, 16:28', 'out', 'video'],
+  ['C', '(+351) 333 222 111', 'December 21, 23:49', 'out', 'video'],
+  ['D', '(+351) 987 654 321', 'December 18, 19:01', 'in', 'video'],
+  ['E', '(+351) 960 000 000', 'October 3, 10:33', 'in', 'voice'],
+];
 
 class CallsPage extends StatelessWidget {
   const CallsPage({Key? key}) : super(key: key);
@@ -7,25 +16,33 @@ class CallsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 11, // Replace with actual number of calls
+        itemCount: callHistory.length,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
             leading: CircleAvatar(
-              child: Text(
-                  'U$index'), // Replace with actual user's initials or profile picture
+              child: Text(callHistory[index][0]),
             ),
-            title: Text('User $index'), // Replace with actual user's name
-            subtitle: const Row(
+            title: Text(callHistory[index][1]),
+            subtitle: Row(
               children: <Widget>[
-                Icon(Icons.call_received,
-                    color:
-                        Colors.red), // Replace with appropriate icon and color
-                Text('10:00'), // Replace with actual call time
+                callHistory[index][3] == 'out'
+                    ? const Icon(Icons.call_made, color: Colors.green, size: 15)
+                    : const Icon(Icons.call_received,
+                        color: Colors.red, size: 15),
+                const SizedBox(width: 4),
+                Text(
+                  callHistory[index][2],
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context).colorScheme.outline),
+                ),
               ],
             ),
-            trailing: const Icon(Icons.call), // Icon to initiate a call
+            trailing: callHistory[index][4] == 'voice'
+                ? const Icon(Icons.call)
+                : const Icon(Icons.videocam), // Icon to initiate a call
             onTap: () {
-              // TODO: Initiate a call
+              context.push('/call/${callHistory[index][1]}');
             },
           );
         },
