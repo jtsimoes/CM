b43734dc1b120e5060c504b30062a494bf84863b
João Tomás <jtsimoes@ua.pt>
2023-12-29 04:48:38 +0000

    Updated package imports in globals

--- Flutter/whatz_up/lib/utils/globals.dart
+++ Flutter/whatz_up/lib/utils/globals.dart
@@ -1,9 +1,9 @@
 export 'dart:io';
-export "package:flutter/foundation.dart";
-export "package:flutter/material.dart";
-export "package:flutter/services.dart";
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
 
-export "package:go_router/go_router.dart";
+export 'package:go_router/go_router.dart';
 export 'package:hive/hive.dart';
 export 'package:hive_flutter/hive_flutter.dart';
 export 'package:flutter_locales/flutter_locales.dart';
@@ -12,15 +12,14 @@ export 'package:shake_gesture/shake_gesture.dart';
 export 'package:app_bar_with_search_switch/app_bar_with_search_switch.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:chat_bubbles/chat_bubbles.dart';
-export "package:url_strategy/url_strategy.dart";
 export 'package:audioplayers/audioplayers.dart';
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:story_view/story_view.dart';
 export 'package:whatsapp_story_editor/whatsapp_story_editor.dart';
 export 'package:image_picker/image_picker.dart';
 
-export "package:whatz_up/utils/routes.dart";
-export "package:whatz_up/utils/themes.dart";
+export 'package:whatz_up/utils/routes.dart';
+export 'package:whatz_up/utils/themes.dart';
 
 export 'package:whatz_up/models/story.dart';
 export 'package:whatz_up/utils/boxes.dart';
